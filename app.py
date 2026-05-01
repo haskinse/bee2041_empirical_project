142b9db1272d35cac9a85c8520818f42a5751e1f
haskinse <elh253@exeter.ac.uk>
2026-05-01 20:05:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("What Makes a Taylor Swift Song Successful?")
 
 st.write("This project explores whether musical features explain song success.")
 
-st.image("output/figures/commercial_succes/total_units_bar.png", caption="My chart")
+st.image("output/figures/commercial_success/total_units_bar.png", caption="My chart")
 
 if st.button("Show insight"):
     st.write("Higher energy songs tend to have more streams.")
