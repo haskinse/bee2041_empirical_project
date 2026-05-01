060127c9b688a1e0a4451678a729a71f3b340682
haskinse <elh253@exeter.ac.uk>
2026-05-01 22:55:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.title("What Makes a Taylor Swift Song Successful?")
 
-st.write("This project explores whether musical features explain song success.")
+st.write("""Taylor Swift is one of the most successful music artists of all time. She’s sold over 100 million album units, multiple of her songs have passed one billion streams, and she’s the highest-grossing live music artist. But what actually drives her success? Is there something in the music itself that explains it, or is it something else entirely?
+
+This blog looks at the musical features of her songs to see whether certain characteristics are linked to success. Are there patterns in which tracks become hugely popular while others don’t, or is her success about more than just the music?""")
 
 st.image("output/figures/commercial_success/total_units_bar.png", caption="My chart")
 
