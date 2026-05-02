7f377befd9afded4a67cfeaa8adeade01a336a16
haskinse <elh253@exeter.ac.uk>
2026-05-02 21:07:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ st.write("""Looking at individual tracks, a small number of songs dominate total
 
 Among the very top songs, 1989 stands out in particular, although there is still a fairly balanced spread across albums overall. As before, older songs have had more time to build up listens, so this likely explains part of the pattern.""")
 
+st.image("output/figures/commercial_success/top_tracks/top_10_track_listeners_table.png")
+
 top_n = st.slider("Choose how many top songs to show:", min_value = 10, max_value = 220, value = 20, step = 10, key = "top_tracks_slider")
 
 st.image(f"output/figures/commercial_success/top_tracks/top_{top_n}_track_listens.png")
